cb5f1202b746da97cfb0fc5a8dfc2ddaf876123a
Ruihuang Yang <rxy216@case.edu>
2024-10-24 20:40:34 -0400

    Add favicon link to the head section in layout.tsx

--- cloud/frontend/xlab-cloud-workshop-chatbot/app/layout.tsx
+++ cloud/frontend/xlab-cloud-workshop-chatbot/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`text-foreground bg-background ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
